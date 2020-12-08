151d8285d85af61bd4d5bf86a71fed55d193f440
subbus <subbareddyg83@gmail.com>
2020-12-08 19:25:11 +0530

    modify code and add comments

--- problem-solving/time-conversion.cpp
+++ problem-solving/time-conversion.cpp
@@ -1,18 +1,24 @@
 //https://www.hackerrank.com/challenges/time-conversion/problem
 #include <bits/stdc++.h>
 using namespace std;
-void solve(string &s)
+string solve(string &s)
 {
-
+    //extracting hrs
     int hrs = stoi(s.substr(0, 2)) % 12;
 
+    //checking if last 2nd character is P or not
     if (s[s.length() - 2] == 'P')
     {
         hrs += 12;
     }
+    stringstream ss(to_string(hrs));
+    //pads with 0 if variable is single digit for eg 5 is padded as 05
+    ss << setw(2) << setfill('0') << hrs;
+    //replacing hrs with modified hrs
+    s.replace(0, 2, ss.str());
     s.pop_back();
     s.pop_back();
-    cout << setw(2) << setfill('0') << hrs << s.substr(2, s.size() - 2);
+    return s;
 }
 int main()
 {
@@ -22,7 +28,7 @@ int main()
 
     string s;
     cin >> s;
-    solve(s);
+    cout << solve(s);
 
     return 0;
 }
