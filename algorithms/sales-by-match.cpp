9dfac6a683b4e6324747bb357ec36c35f1e2a7ee
subbus <subbareddyg83@gmail.com>
2020-12-16 20:47:20 +0530

    add sales-by-match.cpp

---
+++ algorithms/sales-by-match.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Complete the sockMerchant function below.
+int sockMerchant(int n, vector<int> ar)
+{
+    map<int, int> freq;
+    for (auto &x : ar)
+    {
+        freq[x] = 0;
+    }
+    for (auto &x : ar)
+    {
+        freq[x]++;
+    }
+    int count = 0;
+    for (auto &x : freq)
+    {
+        count += x.second / 2;
+    }
+    return count;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &x : v)
+    {
+        cin >> x;
+    }
+    cout << sockMerchant(n, v);
+
+    return 0;
+}
